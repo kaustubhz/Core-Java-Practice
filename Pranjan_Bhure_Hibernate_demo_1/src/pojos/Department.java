d68173c0db6b48f2f7c0de30ac7ade518c4c18b3
Kaustubh Zagade <kaustubhz.kz153@gmail.com>
2019-11-17 14:32:38 +0530

    Hibernate practice assignments

---
+++ Pranjan_Bhure_Hibernate_demo_1/src/pojos/Department.java
@@ -0,0 +1,11 @@
+package pojos;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name="department")
+public class Department {
+
+	private String departmentName;
+	
+}
---
+++ Pranjan_Bhure_Hibernate_demo_1/src/tester/TestMain.java
@@ -0,0 +1,17 @@
+package tester;
+import static utils.HibernateUtils.*;
+import org.hibernate.*;
+
+public class TestMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		try(SessionFactory objSessionFactory=getSessionFactory()) {
+			System.out.println("\n Hibernate booted");
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+}
